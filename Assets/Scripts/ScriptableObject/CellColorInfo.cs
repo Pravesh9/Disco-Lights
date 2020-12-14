33d48a81dd951f11e881ccd8426f80ea5b5fa0fc
Pravesh9 <thebukun@gmail.com>
2020-12-14 22:57:10 +0530

    CellColorInfo Added
    This is a scriptable object used to store different color data related to cells event

---
+++ Assets/Scripts/ScriptableObject/CellColorInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="NewCellColorInfo",menuName ="CellColorInfo")]
+public class CellColorInfo : ScriptableObject
+{
+    public Color selectedCellColor;
+    public Color activeCellColor;
+    public Color highlightedCellColor;
+    public Color deadCellColor;
+
+}
