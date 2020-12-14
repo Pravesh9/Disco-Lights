4e597dcb3f7370f741dc3ee1b6e6abbc1c160ee8
Pravesh9 <thebukun@gmail.com>
2020-12-14 22:35:04 +0530

    Cell Script Added

---
+++ Assets/Scripts/Cell/Cell.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Cell : MonoBehaviour
+{
+    public Vector2 Index;
+    public bool IsActive;
+    
+    
+
+    private Color initalColor;
+    public SpriteRenderer SR;
+
+    
+    
+    void Start()
+    {
+        
+    }
+
+    public void Initialize(Vector2 _index,Color _initialCol)
+    {
+        Index = _index;
+        initalColor = _initialCol;
+
+        SR.color = initalColor;
+
+    }
+
+}
