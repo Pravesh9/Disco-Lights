fb66532d6cf7056743100d12f6845633b50a51f2
Pravesh9 <thebukun@gmail.com>
2020-12-14 22:37:12 +0530

    Board Script Added
    It is a Non- Monobehaviour class used to create the board and it have complete details of all cells present on screen.

---
+++ Assets/Scripts/Board/Board.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Board 
+{
+    public readonly int row; //Rows in a board
+    public readonly int col;//Cols ina a board
+    public Cell[,] AllCells;
+    
+    public Board(int _row,int _col)
+    {
+        row = _row;
+        col = _col;
+        AllCells = new Cell[col, row];
+    }
+
+}
