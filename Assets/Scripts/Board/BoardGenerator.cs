9b62f10d933e1923054f514fd9fb217b06aef087
Pravesh9 <thebukun@gmail.com>
2020-12-14 22:39:13 +0530

    Board Generator Script Added
    It is used to generate the board of different sizes (row,col)

---
+++ Assets/Scripts/Board/BoardGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardGenerator : MonoBehaviour
+{
+    public int row;
+    public int col;
+
+    public Board board;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        board = new Board(row, col);
+        BoardView.instance.DrawGameBoard(ref board);
+    }
+
+    
+}
