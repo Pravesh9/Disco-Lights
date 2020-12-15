5c8e0fb7844a24b5fd9176af78b8a20c5b2809ea
Pravesh9 <thebukun@gmail.com>
2020-12-15 15:43:10 +0530

    Extension Method Added
    New DebugManager Added (Colorful console message)

---
+++ Assets/Scripts/ExtensionMethod/DebugManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugManager : Debug
+{
+    public static void LogWithColor(string _message, Color _color)
+    {
+        Debug debug = new Debug();
+        debug.LogWithColor(_message, _color);
+    }
+
+}
---
+++ Assets/Scripts/ExtensionMethod/ExtensionMethod.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethod
+{
+
+    public static List<T> ShuffleList<T>(this List<T> _list)
+    {
+
+        for (int i = 0; i < 100; i++)
+        {
+            int r = Random.Range(0, _list.Count);
+            int p = Random.Range(0, _list.Count);
+            T temp = _list[r];
+            _list[r] = _list[p];
+            _list[p] = temp;
+        }
+
+        return _list;
+    }
+
+    public static void LogWithColor(this Debug _g, string _msg, Color _color)
+    {
+        Debug.Log("<Color=#" + ColorUtility.ToHtmlStringRGBA(_color) + ">" + _msg + "</Color>");
+    }
+
+    public static void Visible(this GameObject _gameObject)
+    {
+        _gameObject.SetActive(true);
+    }
+
+    public static void InVisible(this GameObject _gameObject)
+    {
+        _gameObject.SetActive(false);
+    }
+
+}
+
+
+
+
