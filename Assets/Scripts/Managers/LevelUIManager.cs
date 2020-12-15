0fd39821d612740b04ff785ee57c742d8deaf1c2
Pravesh9 <thebukun@gmail.com>
2020-12-15 16:54:02 +0530

    LevelUIManager Added
    It is responsible for UI Panels

---
+++ Assets/Scripts/Managers/LevelUIManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUIManager : MonoBehaviour
+{
+    #region Public Variables
+    public static LevelUIManager instance;
+    public GameObject pausePanel;
+    public GameObject winPanel;
+    #endregion
+
+    #region MonoBehaviour Method
+    private void Awake() => instance = this;
+
+    private void OnEnable()
+    {
+        GameManager.OnPlayerWon += PlayerWon;
+        GameManager.OnGamePause += Pause;
+        GameManager.OnGameResume += Resume;
+    }
+    private void OnDisable()
+    {
+        GameManager.OnPlayerWon -= PlayerWon;
+        GameManager.OnGamePause -= Pause;
+        GameManager.OnGameResume -= Resume;
+    }
+    #endregion
+
+    #region OtherMethod
+    void Pause()
+    {
+        pausePanel.Visible();
+    }
+
+    void Resume()
+    {
+        pausePanel.InVisible();
+    }
+
+    void PlayerWon()
+    {
+        winPanel.Visible();
+    }
+    #endregion 
+}
+
+
