f38d6855be508674ae1c107b8a3d13fe8f748b26
Pravesh9 <thebukun@gmail.com>
2020-12-15 12:06:40 +0530

    GameManager Added

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public static event Action OnPlayerWon;
+
+    private void Awake() => instance = this;
+
+    public void PlayerWon()
+    {
+        print("PlayerWon");
+        OnPlayerWon?.Invoke();
+    }
+
+}
