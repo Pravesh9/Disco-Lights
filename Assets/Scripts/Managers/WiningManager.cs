abb60689ef4357637667d8f48a2bb4ff1648ad4b
Pravesh9 <thebukun@gmail.com>
2020-12-15 12:08:51 +0530

    Wining Manager Added
    It is responsible for wining concept of game.

---
+++ Assets/Scripts/Managers/WiningManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WiningManager : MonoBehaviour
+{
+    public static WiningManager instance;
+
+    private void Awake() => instance = this;
+
+    public void CheckWining(Cell[,] cells)
+    {
+        for (int i = 0; i < cells.GetLength(0); i++)
+        {
+            for (int j = 0; j < cells.GetLength(1); j++)
+            {
+                if (cells[i, j].IsActive)
+                {
+                    return; //It means There is some cell remaining which is still alive.
+                }
+            }
+        }
+        if(GameManager.instance!=null)
+        GameManager.instance.PlayerWon();
+    }
+
+
+}
