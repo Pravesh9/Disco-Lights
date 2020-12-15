5d23741543e7caa2c30663341bc93cdf4c8547ef
Pravesh9 <thebukun@gmail.com>
2020-12-15 15:42:04 +0530

    AudioManager Added
    This is responsible for audio effect during the gameplay

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        GameManager.OnPlayerWon += PlayerWon;
+    }
+    private void OnDisable()
+    {
+        GameManager.OnPlayerWon -= PlayerWon;
+    }
+
+    void PlayerWon() => DebugManager.LogWithColor($"{this.name}: You can add here wining sound",Color.blue); 
+
+}
